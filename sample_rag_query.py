74ee66fb3fed82e963718ff59fee75b416946742
isayhc <isayahculbertson@gmail.com>
2024-01-12 14:59:22 -0500

    adding needed files to run this code

--- sample_rag_query.py
+++ sample_rag_query.py
@@ -1,20 +1,24 @@
 import os
 from llama_index import StorageContext, load_index_from_storage
 from dotenv import load_dotenv
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 load_dotenv()
 
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+LIBRARY_DIRECTORY = os.geteLIBRARY_DIRECTORY
 
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
-documents = SimpleDirectoryReader("sample").load_data()
+documents = SimpleDirectoryReader(LIBRARY_DIRECTORY).load_data()
 index = VectorStoreIndex.from_documents(documents)
 
 index.storage_context.persist()
 
 # rebuild storage context
-storage_context = StorageContext.from_defaults(persist_dir="./storage")
+# storage_context = StorageContext.from_defaults(persist_dir="./storage")
 # load index
-index = load_index_from_storage(storage_context)
+# index = load_index_from_storage(storage_context)
+
+# query_engine = index.as_query_engine()
+# query_engine.query("YOUR_QUESTION")
