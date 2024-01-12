926091ce5ed4293b9fd776e2e5b8d1e1399bb2ca
isayhc <isayahculbertson@gmail.com>
2024-01-12 00:48:34 -0500

    modifying to be used as a script for an agent

--- query_documents.py
+++ query_documents.py
@@ -1,11 +1,10 @@
+import argparse
 import os
 from llama_index import StorageContext, load_index_from_storage
 from dotenv import load_dotenv
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
-
-
-def query_data(query:str):
+def query_data(query: str):
     """Query to a vector database
     
     ## argument
@@ -17,4 +16,17 @@ def query_data(query:str):
     index = load_index_from_storage(storage_context)
 
     query_engine = index.as_query_engine()
-    query_engine.query(query)
+    user_query = query_engine.query(query)
+    user_query = user_query.response
+    return user_query
+    # x = 0
+
+def main():
+    parser = argparse.ArgumentParser(description='Query a vector database.')
+    parser.add_argument('query', type=str, help='Query to be executed')
+
+    args = parser.parse_args()
+    query_data(args.query)
+
+if __name__ == "__main__":
+    main()
